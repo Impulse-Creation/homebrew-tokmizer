819ebcc044b6a8702da9c7d6f2224de87672e36a
Impulse-Creation <luc.masclaux@impulse-creation.fr>
2026-06-10 22:07:00 +0200

    tokmizer 0.5.6

--- Formula/tokmizer.rb
+++ Formula/tokmizer.rb
@@ -1,8 +1,8 @@
 class Tokmizer < Formula
   desc "Cuts the tokens your AI coding agent burns on command output"
   homepage "https://tokmizer.com"
-  url "https://registry.npmjs.org/@tokmizer/plugin/-/plugin-0.5.5.tgz"
-  sha256 "ab0e5fd18fc8f8a0b66eb3620d30e3632adb10bd4e9c8295acc6a4226d751ce7"
+  url "https://registry.npmjs.org/@tokmizer/plugin/-/plugin-0.5.6.tgz"
+  sha256 "6e6cd699d3340b04fa21932fcae967c4309f30e9c1e8f4484de402553a55f3f2"
   license :cannot_represent
 
   depends_on "node"
