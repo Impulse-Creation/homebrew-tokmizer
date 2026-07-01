b7a51910393ae0c429fc7275be819a9caac73661
Impulse-Creation <luc.masclaux@impulse-creation.fr>
2026-07-01 18:13:07 +0200

    chore: bump formula to 0.9.5

--- Formula/tokmizer.rb
+++ Formula/tokmizer.rb
@@ -1,8 +1,8 @@
 class Tokmizer < Formula
   desc "Cuts the tokens your AI coding agent burns on command output"
   homepage "https://tokmizer.com"
-  url "https://registry.npmjs.org/@tokmizer/plugin/-/plugin-0.9.4.tgz"
-  sha256 "6484b417d57c146fe4f945a3077fab3a4de1287a5a6ae56e6529084dea32e1bf"
+  url "https://registry.npmjs.org/@tokmizer/plugin/-/plugin-0.9.5.tgz"
+  sha256 "89e5c186c84a1e7d87847c144b966072ae86895ac58a86600fd4532dcb468e48"
   license :cannot_represent
 
   depends_on "node"
